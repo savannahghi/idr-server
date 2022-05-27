b82f8514db25e771b7703f6ac3dd09470989699f
Kennedy Kori <kennedykori47@gmail.com>
2022-05-27 23:38:37 +0300

    chore: Add production config

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.views.generic import RedirectView
 
@@ -25,4 +27,9 @@ urlpatterns = [
     path("api/", include("apps.core.urls")),
     path("api/", include("apps.sql_sources.urls")),
     path(settings.ADMIN_URL, admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    # Static file serving when using Gunicorn + Uvicorn for local web socket development
+    urlpatterns += staticfiles_urlpatterns()
+
