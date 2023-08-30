eb3bb5901948b4a1197df7abb6b3c315305285ea
Salad <saladguyo60@gmail.com>
2023-08-30 12:26:18 +0300

    chore(build): add switch deployment strategy from cloud run to Kubernetes (#68)
    * feat: switch deployment strategy from cloud run to Kubernetes
    
    - replacement of  task in cloudbuild file with
    - addition of  file with the relevant kubernetes configurations
    
    switch idr-server deployment strategy
    
    * refactor: modularize k8ts services for flexibility of changes
    
    * chore: increase pod replicas
    Increase django service replicas from 1 to 2
    Increase PgBouncer service replicas from 1 to 2

--- config/settings/local.py
+++ config/settings/local.py
@@ -16,7 +16,7 @@ DATABASES = {
         "NAME": env.str("POSTGRES_DB"),
         "USER": env.str("POSTGRES_USER"),
         "PASSWORD": env.str("POSTGRES_PASSWORD"),
-        "HOST": env.str("POSTGRES_HOST", defualt="localhost"),
+        "HOST": env.str("POSTGRES_HOST", default="localhost"),
         "PORT": env.int("POSTGRES_PORT", default=5432),
         "ENGINE": "django.contrib.gis.db.backends.postgis",
         "ATOMIC_REQUESTS": True,
